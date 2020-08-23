dd872155b47bee07812966c3de32be0833108333
Woojung Lee <wjlee612@gmail.com>
2020-08-23 21:36:58 +0900

    preprocess.py

--- 4_Word2Vec/preprocess.py
+++ 4_Word2Vec/preprocess.py
@@ -19,9 +19,10 @@ def checkCorpus(datapath):
 
 
 class Preprocess(object):
-	def __init__(self, datapath='./data/corpus.txt', window_size=5):
+	def __init__(self, datapath='./data/corpus.txt', window_size=5, kor = False):
 		self.datapath = datapath 
 		self.window_size = window_size
+		self.kor = kor
 
 	def split_sentences(self):
 		sentences = []
@@ -30,18 +31,22 @@ class Preprocess(object):
 			while True:
 				line = corpus.readline()
 				if not line: break
-				sentences.append(re.sub(r"[^a-z0-9]+", " ", line.lower())[:-1])
+				if not self.kor:
+					sentences.append(re.sub(r"[^a-z0-9]+", " ", line.lower())[:-1])
+				else:
+					sentences.append(line)
 		else:
 			line = corpus.readline()
 			sentence = line.split(". ")
 			for sent in sentence:
-				sentences.append(re.sub(r"[^a-z0-9]+", " ", sent.lower()))
+				if not self.kor:
+					sentences.append(re.sub(r"[^a-z0-9]+", " ", sent.lower()))
+				else:
+					sentences.append(sent)
 		return sentences
 
 	def build_data(self, max_vocab):
 		sentences = self.split_sentences()
-		print("-"*30)
-		print("Building data...", end = ' ')
 		self.unk = '<unk>'
 		self.word_count = {self.unk: 1}
 		for idx, sentence in enumerate(sentences):
@@ -50,14 +55,14 @@ class Preprocess(object):
 					self.word_count[word] = 1
 				else:
 					self.word_count[word] += 1
-			print("'\r{0}th sentence.".format(idx+1), end = '')
+			if not (idx+1) % 1000:
+				print("Building {}kth sentence to data...".format((idx+1)//1000), end = '\r')
 
 		self.idx2word = [self.unk] + [key for key, _ in sorted(self.word_count.items(), key = (lambda x: x[1]), reverse=True)][:max_vocab-1]
 		self.word2idx = {word: index for index, word in enumerate(self.idx2word)}
 		self.vocab = set(list(self.word2idx))
 
-		print("DONE!")
-		print("-"*30)
+		print("")
 		print("Save the data...", end = '')
 
 		pickle.dump(self.word_count, open('wordcount.dat','wb'))
@@ -80,11 +85,13 @@ class Preprocess(object):
 		return center_idx, contexts_idx
 
 	def build_training_data(self):
-		print("-"*30)
-		print("Building training data...", end = ' ')
 		data = []
+		cnt = 0
 		sentences = self.split_sentences()
 		for sentence in sentences:
+			cnt += 1
+			if not cnt % 1000:
+				print("Building {}kth sentence to training data using skipgram...".format(cnt//1000), end ='\r')
 			sent = []
 			for idx, word in enumerate(sentence.split(" ")):
 				if word not in self.vocab:
@@ -95,6 +102,6 @@ class Preprocess(object):
 				center, contexts = self.skipgram(sent, idx)
 				data.append((center, contexts))
 
-		pickle.dump(data, open('training_data.dat','wb'))
-		print("DONE!")
+		pickle.dump(data, open('training_data.dat','wb'), protocol = 4)
+		print("")
 		return
