6b2e95a47a692cfe71b8109402253cd9f39bc831
Woojung Lee <wjlee612@gmail.com>
2020-09-01 21:56:18 +0900

    preprocess

--- 5_Seq2Seq/preprocess.py
+++ 5_Seq2Seq/preprocess.py
@@ -2,14 +2,26 @@
 
 import os
 import spacy
+import dill
+from konlpy.tag import Mecab
 
 from torchtext.datasets import Multi30k
 from torchtext.data import Field, TabularDataset
 
+spacy_en = spacy.load('en')
+mecab = Mecab()
+
+def eng_tokenizer(text):
+	return [tok.text for tok in spacy_en.tokenizer(text)][::-1]
+
+def kor_tokenizer(text):
+	return [tok for tok in mecab.morphs(text)]
+
 class Preprocess(object):
 
-	def __init__(self, dpath = './dataset/',
-				 src_tokenizer = None, trg_tokenizer = None):
+	def __init__(self, src_tokenizer = eng_tokenizer,
+				 trg_tokenizer = kor_tokenizer, dpath = './dataset/',
+				 min_freq = 5):
 
 		self.dpath = dpath
 		self.train_path = 'train_Corpus.csv'
@@ -17,6 +29,7 @@ class Preprocess(object):
 		self.test_path = 'test_Corpus.csv'
 		self.src_tokenizer = src_tokenizer
 		self.trg_tokenizer = trg_tokenizer
+		self.min_freq = min_freq
 
 	def Build(self):
 
@@ -31,6 +44,9 @@ class Preprocess(object):
 		tr, val, ts = TabularDataset.splits(
 				path = self.dpath, train = self.train_path, validation = self.valid_path,
 				test = self.test_path, format = 'csv',
-				fields = [('SRC',SRC), ('TRG', TRG)])
+				fields = [("SRC",SRC), ("TRG",TRG)])
 
-		return tr, val, ts 
+		SRC.build_vocab(tr, min_freq = self.min_freq)
+		TRG.build_vocab(tr, min_freq = self.min_freq)
+
+		return SRC, TRG, tr, val, ts
