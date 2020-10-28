861ccad3c159d0a6f6c7d089f6bb61c70e44fcd8
Woojung Lee <wjlee612@gmail.com>
2020-10-28 21:48:38 +0900

    1.(g)

---
+++ 3_CS224n/Lecture10/a5/cnn.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+CS224N 2019-20: Homework 5
+"""
+
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    # Remember to delete the above 'pass' after your implementation
+    ### YOUR CODE HERE for part 1g
+
+    def __init__(self, k, e_char, e_word, batch_size):
+
+        self.Conv1D = nn.Conv1d(in_channels = e_char, output_channels = e_word,
+				kernel_size = k, bias = True)
+        self.relu = nn.ReLU()
+
+	def __forward__(self, x_reshaped):
+		# x_reshaped : [e_char, m_word, batch_size]
+        x_reshaped = x_reshaped.permute(2,0,1)
+        # x_reshape : [batch size, e_char, m_word]
+
+        # W : [e_word, e_char, k], b : [e_word] (e_word: number of filters, k : kernerl size)
+        # x_conv(i,t) = sum(W[i,:,:] ** x_reshaped[:,t:t+k-1]) + b_i
+        # ---> x_conv = Conv1D(x_reshaped)
+        x_conv = self.Conv1D(x_reshape)
+        # x_conv : [batch size, e_word, m_word-k+1]
+        
+        # x_conv_out = MaxPool(ReLU(x_conv))
+        x_conv_out, _ = torch.max(self.relut(x_conv),2)
+        # x_conv_out : [e_word, batch_size]
+        
+        return x_conv_out.permute(1,0)
+
+    ### END YOUR CODE
+
