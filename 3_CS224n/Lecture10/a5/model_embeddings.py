9eca6a1d4ab1b04159f24f049f8267742646f191
Woojung Lee <wjlee612@gmail.com>
2020-11-03 22:39:55 +0900

    1.(h)

--- 3_CS224n/Lecture10/a5/model_embeddings.py
+++ 3_CS224n/Lecture10/a5/model_embeddings.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
-CS224N 2019-20: Homework 5
+CS224N 2018-19: Homework 5
 model_embeddings.py: Embeddings for the NMT model
 Pencheng Yin <pcyin@cs.cmu.edu>
 Sahil Chopra <schopra8@stanford.edu>
@@ -11,6 +10,7 @@ Michael Hahn <mhahn2@stanford.edu>
 """
 
 import torch.nn as nn
+import torch
 
 # Do not change these imports; your module names should be
 #   `CNN` in the file `cnn.py`
@@ -20,74 +20,77 @@ import torch.nn as nn
 from cnn import CNN
 from highway import Highway
 
-
 # End "do not change"
-import torch
+
 
 class ModelEmbeddings(nn.Module):
     """
     Class that converts input words to their CNN-based embeddings.
     """
 
-    def __init__(self, word_embed_size, vocab):
+    def __init__(self, embed_size, vocab):
         """
         Init the Embedding layer for one language
-        @param word_embed_size (int): Embedding size (dimensionality) for the output word
+        @param embed_size (int): Embedding size (dimensionality) for the output 
         @param vocab (VocabEntry): VocabEntry object. See vocab.py for documentation.
-
-        Hints: - You may find len(self.vocab.char2id) useful when create the embedding
         """
         super(ModelEmbeddings, self).__init__()
 
-        ### YOUR CODE HERE for part 1h
+        ## A4 code
+        # pad_token_idx = vocab.src['<pad>']
+        # self.embeddings = nn.Embedding(len(vocab.src), embed_size, padding_idx=pad_token_idx)
+        ## End A4 code
 
-        self.vocab = vocab
-        self.e_word = word_embed_size
-        self.e_char = 50
-        self.kernel = 5
-        self.dropout_prob = 0.3
-        self.pad_token = vocab.char2id['<pad>']
-        self.embedding = nn.Embedding(num_embeddings = len(vocab.char2id),
-				                      embedding_dim = self.e_char,
-									  padding_idx = self.pad_token)
-        self.cnn = CNN(self.kernel, self.e_char, self.e_word)
-        self.highway = Highway(self.e_word, self.dropout_prob)
-        self.dropout = nn.Dropout(self.dropout_prob)
+        ### YOUR CODE HERE for part 1j
+
+        # there is two problems
+        # 1. Now that embed_size is for output,
+        # so why A4 code take embed_size as param for self.embeddings?
+        # remember we take e_{char} = 50
+        # 2. VocabEntry object doesn't own the attribute 'src'
+
+
+        pad_token_idx = vocab.char2id['<pad>']
+        self.embed_size = embed_size
+        char_embed_size = 50
+        self.char_embedding = nn.Embedding(len(vocab.char2id),
+                                           char_embed_size,
+                                           pad_token_idx)
+        self.convNN = CNN(f=self.embed_size)
+        self.highway = Highway(embed_size=self.embed_size)
+        self.dropout = nn.Dropout(p=0.3)
 
         ### END YOUR CODE
 
-    def forward(self, input):
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
         """
         Looks up character-based CNN embeddings for the words in a batch of sentences.
         @param input: Tensor of integers of shape (sentence_length, batch_size, max_word_length) where
             each integer is an index into the character vocabulary
-
-        @param output: Tensor of shape (sentence_length, batch_size, word_embed_size), containing the
+        @param output: Tensor of shape (sentence_length, batch_size, embed_size), containing the 
             CNN-based embeddings for each word of the sentences in the batch
         """
-        ### YOUR CODE HERE for part 1h
+        ## A4 code
+        # output = self.embeddings(input)
+        # return output
+        ## End A4 code
 
-        # input : [sentence length, batch size, m_word]
-        # x_emb = CharEmbedding(x_padded), x_padded = input
+        ### YOUR CODE HERE for part 1j
+        
+        X_word_emb_list = []
+        # divide input into sentence_length batchs
+        for X_padded in input:
+            X_emb = self.char_embedding(X_padded)
+            X_reshaped = torch.transpose(X_emb, dim0=-1, dim1=-2)
+            # conv1d can only take 3-dim mat as input
+            # so it needs to concat/stack all the embeddings of word
+            # after going through the network
+            X_conv_out = self.convNN(X_reshaped)
+            X_highway = self.highway(X_conv_out)
+            X_word_emb = self.dropout(X_highway)
+            X_word_emb_list.append(X_word_emb)
 
-        x_emb = self.embedding(input)
-        # x_emb : [sentence length, batch size, m_word, e_char]
+        X_word_emb = torch.stack(X_word_emb_list)
+        return X_word_emb
 
-        x_reshaped = x_emb.permute(0,3,2,1)
-        # x_reshaped = [sentence length, e_char, m_word, batch size]
-
-        sen_len, batch_size, m_word = x_emb.shape[0], x_emb.shape[1], x_emb.shape[2]
-
-        tot_word_emb = []
-        for x in x_reshaped:
-            # x : [e_char, m_word, batch_size]
-            x_conv_out = self.cnn(x) # x_conv_out: [batch size, e_word]
-            x_word_emb = self.highway(x_conv_out) # x_word_emb : [batch size, e_word]
-            x_word_emb = self.dropout(x_word_emb)
-            tot_word_emb.append(x_word_emb)
-
-        x_word_emb = torch.stack(tot_word_emb)
-
-        return x_word_emb
         ### END YOUR CODE
-
